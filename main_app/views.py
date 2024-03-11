41a17dbb4fc51bc6cbf88a8652600de1dc88083b
Jessica <jessicamendez328@gmail.com>
2024-03-11 16:36:06 -0400

    about page and plant index route & dynamic render

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+plants = [
+    {'common_name': 'ZZ Plant', 'botanical_name': 'Zamioculcas zamiifolia', 'sunlight': 'Partial', 'water': 'when soil is bone dry', 'leaves': 'Green, shiny'},
+    {'common_name': 'Monstera', 'botanical_name': 'Monstera acacoyaguensis', 'sunlight': 'Bright and Indirect', 'water': 'when top of soil is dry', 'leaves': 'Green, shiny'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+  return render(request, 'plants/index.html', {
+    'plants': plants
+  })
