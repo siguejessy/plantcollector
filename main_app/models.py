775f3912da57d607e0a2b24558b1472dd348bbfc
Jessica <jessicamendez328@gmail.com>
2024-03-13 15:05:53 -0400

    edit plant details functionality

--- main_app/models.py
+++ main_app/models.py
@@ -8,9 +8,9 @@ class Plant(models.Model):
     water_need = models.TextField(max_length=500)
     soil_type = models.TextField(max_length=500)
     leaf_description = models.TextField(max_length=500)
-    toxic = models.BooleanField(default=True)
     native_to = models.TextField(max_length=250)
     acquired_via = models.TextField(max_length=250) 
+    toxic = models.BooleanField(default=True)
 
 
     def __str__(self):
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('plants/', views.plants_index, name='index'),
   path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
+  path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
 ]
