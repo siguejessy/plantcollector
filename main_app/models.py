92058e44a74dd9d34d27a1f306f64d2efa7dec0d
Jessica <jessicamendez328@gmail.com>
2024-03-13 15:38:11 -0400

    redirect to new plant details page after new plant is added to collection

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Plant(models.Model):
@@ -16,3 +17,5 @@ class Plant(models.Model):
     def __str__(self):
         return f'{self.common_names} ({self.id})'
     
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'plant_id': self.id})
